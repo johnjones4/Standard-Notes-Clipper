5d5522765ee263890a023815581015ffed994022
John Jones <johnjones4@gmail.com>
2019-09-29 17:46:31 -0400

    added tag sync

--- background.js
+++ background.js
@@ -64,7 +64,9 @@ const logout = window.logout = () => {
     chrome.storage.sync.set({
       token: null,
       params: null,
-      keys: null
+      keys: null,
+      tagSyncToken: null,
+      tags: []
     }, () => {
       resolve()
     })
@@ -129,6 +131,67 @@ const saveClipping = (content, {params, keys}) => {
       console.log(result)
     })
 }
+
+const syncTags = () => {
+  const fetchTags = (keys, syncToken, cursorToken, tags) => {
+    return snRequest(true, 'items/sync', 'POST', {
+      items: [],
+      sync_token: syncToken,
+      cursor_token: cursorToken
+    })
+      .then(response => {
+        const newTags = []
+        response.retrieved_items.forEach(item => {
+          if (item.content_type === 'Tag') {
+            if (item.deleted) {
+              delete tags[item.uuid]
+            } else {
+              newTags.push(item)
+            }
+          }
+        })
+        return Promise.all(
+          newTags.map(tag => {
+            return SFJS.itemTransformer.decryptItem(tag, keys)
+              .then(() => {
+                const content = JSON.parse(tag.content)
+                tags[tag.uuid] = content.title
+              })
+          })
+        ).then(() => response)
+      })
+      .then(response => {
+        if (response.cursor_token) {
+          return fetchTags(keys, syncToken, response.cursor_token, tags)
+        } else {
+          return {
+            tags,
+            syncToken: response.sync_token
+          }
+        }
+      })
+  }
+  return (() => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.get({
+        keys: null,
+        tagSyncToken: null,
+        tags: {}
+      }, (items) => resolve(items))
+    })
+  })()
+    .then(({tagSyncToken, tags, keys}) => {
+      return fetchTags(keys, tagSyncToken, null, tags)
+    })
+    .then(({tags, syncToken}) => {
+      return new Promise((resolve, reject) => {
+        chrome.storage.sync.set({
+          tagSyncToken: syncToken,
+          tags: tags
+        },() => resolve(tags))
+      })
+    })
+}
   
 const checkForUser = () => {
   return new Promise((resolve) => {
@@ -146,3 +209,8 @@ const checkForUser = () => {
 }
 
 checkForUser()
+  .then(items => {
+    if (items.token) {
+      syncTags().then(tags => console.log(tags))
+    }
+  })
