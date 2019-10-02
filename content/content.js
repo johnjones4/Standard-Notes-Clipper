352938c23358c3912600f0566f14bbc05649ac8c
John Jones <johnjones4@gmail.com>
2019-10-01 23:09:36 -0400

    starting clipping ui

--- content/content.js
+++ content/content.js
@@ -1,14 +1,16 @@
 (() => {
+  let clipper = null
+
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     switch (request.type) {
       case 'clip':
-        startClipper().then(content => {
-          sendResponse({
-            title: getTitle(),
-            url: getURL(),
-            text: content
-          })
-        })
+        try {
+          startClipper()
+            .then(content => sendResponse(content))
+            .catch(err => console.error(err))
+        } catch (err) {
+          console.error(err)
+        }
         return true
       case 'done':
         alert('Saved!')
@@ -42,42 +44,165 @@
   }
 
   const startClipper = () => {
-    return new Promise((resolve, reject) => {
-      const hoverEl = makeHoverElement()
-
-      const hoverChanged = (event) => {
-        const el = document.elementFromPoint(event.clientX, event.clientY)
-        if (el) {
-          matchHoverElement(hoverEl, el)
-        }
-      }
-
-      const hoverClicked = (event) => {
-        hoverEl.remove()
-        const el = document.elementFromPoint(event.clientX, event.clientY)
-        const content = el.innerHTML
-        resolve(content)
-      }
-
-      document.body.appendChild(hoverEl)
-      document.addEventListener('mousemove', hoverChanged, false)
-      document.addEventListener('mouseup', hoverClicked, false)
-    })
+    if (clipper) {
+      clipper.detach()
+    }
+    const content = {
+      title: getTitle(),
+      url: getURL(),
+      text: '',
+      tags: []
+    }
+    clipper = new Clipper(content)
+    return clipper.attach()
+      .then(() => {
+        return new Promise((resolve, reject) => {
+          clipper.on('clipped', (content) => {
+            resolve(content)
+          })
+        })
+      })
   }
 
-  const matchHoverElement = (hoverEl, el) => {
-    const rect = el.getBoundingClientRect()
-    const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop
-    hoverEl.style.top = (rect.top + scrollTop) + 'px'
-    hoverEl.style.left = (rect.left + scrollLeft) + 'px'
-    hoverEl.style.width = rect.width + 'px'
-    hoverEl.style.height = rect.height + 'px'
-  }
+  class Clipper {
+    constructor (content) {
+      this.content = content
+      this.handlers = {}
+      this.initHoverElement()
+      this.initControlBoxElement()
+      this.mouseMovedHandler = (event) => this.mouseMoved(event)
+      this.mouseClickedHandler = (event) => this.mouseClicked(event)
+    }
 
-  const makeHoverElement = () => {
-    const hoverEl = document.createElement('div')
-    hoverEl.id = 'standard-notes-clipper-hover'
-    return hoverEl
+    initHoverElement () {
+      this.hoverElement = document.createElement('div')
+      this.hoverElement.id = 'standard-notes-clipper-hover'
+    }
+
+    initControlBoxElement () {
+      this.controlBox = document.createElement('div')
+      this.controlBox.id = 'standard-notes-control-box'
+      this.controlBox.classList = 'mode-1'
+
+      // const title = document.createElement('h1')
+      // title.textContent = 'Clip to Standard Notes'
+      // this.controlBox.appendChild(title)
+
+      const help = document.createElement('p')
+      help.textContent = 'Hover over the segment of the page you wish to clip and click.'
+      help.className = 'section start-direction direction'
+      this.controlBox.appendChild(help)
+
+      const formSection = document.createElement('div')
+      formSection.className = 'section forms'
+      this.controlBox.appendChild(formSection)
+
+      const titleField = document.createElement('input')
+      titleField.type = 'text'
+      titleField.className = 'title' 
+      titleField.value = this.content.title
+      titleField.addEventListener('keyup', () => this.content.title = titleField.value)
+      titleField.addEventListener('change', () => this.content.title = titleField.value)
+      formSection.appendChild(titleField)
+
+      const tagsField = document.createElement('input')
+      tagsField.type = 'text'
+      tagsField.className = 'tags'
+      tagsField.value = this.content.tags.length > 0 ? ('#' + this.content.tags.join(' #')) : ''
+      tagsField.placeholder = '#Tags'
+      tagsField.addEventListener('keyup', () => this.updateTags(tagsField.value))
+      tagsField.addEventListener('change', () => this.updateTags(tagsField.value))
+      formSection.appendChild(tagsField)
+
+      const buttonSection = document.createElement('div')
+      buttonSection.className = 'section buttons'
+      this.controlBox.appendChild(buttonSection)
+
+      const okButton = document.createElement('button')
+      okButton.className = 'save'
+      okButton.textContent = 'Save'
+      buttonSection.appendChild(okButton)
+
+      const cancelButton = document.createElement('button')
+      cancelButton.className = 'skip'
+      cancelButton.textContent = 'Skip'
+      buttonSection.appendChild(cancelButton)
+    }
+
+    updateTags (tagsStr) {
+      this.content.tags = tagsStr
+        .split(' ')
+        .map(tag => tag[0] === '#' ? tag.substring(1) : tag)
+        .filter(tag => tag.trim() !== '')
+    }
+
+    on (event, handler) {
+      if (!this.handlers[event]) {
+        this.handlers[event] = []
+      }
+      this.handlers[event].push(handler)
+    }
+
+    fire (event, info) {
+      if (this.handlers[event]) {
+        this.handlers[event].forEach(handler => handler(info))
+      }
+    }
+
+    attach () {
+      this.shadowDomRoot = document.createElement('div')
+      this.shadowDomRoot.style.zIndex = 100000
+      document.body.appendChild(this.shadowDomRoot)
+      this.shadowDomRoot.attachShadow({mode: 'open'})
+      return fetch(chrome.runtime.getURL('content/content.css'))
+        .then(res => res.text())
+        .then(stylesheet => {
+          const style = document.createElement('style')
+          style.textContent = stylesheet
+          this.shadowDomRoot.shadowRoot.appendChild(style)
+
+          this.shadowDomRoot.shadowRoot.appendChild(this.hoverElement)
+          this.shadowDomRoot.shadowRoot.appendChild(this.controlBox)
+          document.addEventListener('mousemove', this.mouseMovedHandler, false)
+          document.addEventListener('mouseup', this.mouseClickedHandler, false)
+        })
+    }
+
+    detach () {
+      if (this.shadowDomRoot) {
+        this.shadowDomRoot.remove()
+        this.removeListeners()
+      }
+    }
+
+    removeListeners () {
+      document.removeEventListener('mousemove', this.mouseMovedHandler, false)
+      document.removeEventListener('mouseup', this.mouseClickedHandler, false)
+    }
+
+    mouseMoved (event) {
+      const element = document.elementFromPoint(event.clientX, event.clientY)
+      if (element && element !== this.shadowDomRoot) {
+        const rect = element.getBoundingClientRect()
+        const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        this.hoverElement.style.top = (rect.top + scrollTop) + 'px'
+        this.hoverElement.style.left = (rect.left + scrollLeft) + 'px'
+        this.hoverElement.style.width = rect.width + 'px'
+        this.hoverElement.style.height = rect.height + 'px'
+      }
+    }
+
+    mouseClicked (event) {
+      const element = document.elementFromPoint(event.clientX, event.clientY)
+      if (element && element !== this.shadowDomRoot) {
+        this.removeListeners()
+        this.hoverElement.classList.add('selected')
+        this.controlBox.classList.remove('mode-1')
+        this.controlBox.classList.add('mode-2')
+        this.content.text = element.innerHTML
+        this.fire('clipped', this.content)
+      }
+    }
   }
 })()
