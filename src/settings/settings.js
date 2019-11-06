a8673b7dadd079f2f7e16ddd2ba033a3377a4d0a
John Jones <johnj@casefoundation.org>
2019-11-06 14:10:07 -0500

    converted settings.html for webpack

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,4 +1,6 @@
 import { h, render } from 'preact'
 import SettingsPage from './components/SettingsPage'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './settings.css'
 
 render(h(SettingsPage), document.getElementById('options'))
